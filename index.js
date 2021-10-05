aab4af519e302ecf5a60c201d304cd2c723eb1b8
eagyemang21 <elijahagyemang9@gmail.com>
2021-10-05 14:50:05 +0000

    Code challenge 10/05/2021

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 //1
 function fizzBuzz(){
-  
+  for(let i = 1; i <= 100; i ++){
+       if(i % 5 === 0 && i % 3 === 0){
+           console.log("fizzbuzz")
+       }else if(i % 5 === 0){
+           console.log("buzz")
+       }else if(i % 3 === 0){
+           console.log("fizz")
+       }else{
+           console.log(i)
+       }
+  }
 }
-
-//2
+  fizzBuzz()
+  
 function fizzBuzz(number){
-  
+    let answer = []
+    for(let i = 1; i <= number; i++){
+        if(i % 3 === 0 & i % 5 === 0){
+            answer.push("FizzBuzz")
+        }else if(i % 5 === 0){
+            answer.push("Buzz")
+        }else if(i % 3 === 0){
+            answer.push("Fizz")
+        }else{
+            answer.push(`${i}`)
+        }
+    }
+    return answer
 }
-
-
+console.log(fizzBuzz(99))
